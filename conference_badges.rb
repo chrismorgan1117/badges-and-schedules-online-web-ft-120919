88658bbd6ccea470fbd83f22d22905ad976295ed
Christopher Morgan <cwm111794@gmail.com>
2019-12-12 01:00:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each{|name| badges.push("Hello, my name is #{name}.")}
+  return badges
+end 
+
+def assign_rooms(attendees)
+  room_assignments = []
+  counter = 0
+  room = 1 
+  while counter < (attendees.length) do
+    name = attendees.fetch(counter)
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1 
+    counter += 1
+  end 
+  return room_assignments
+end 
+
+def printer(attendees)
+  badges_and_room_assignments = [] 
+   
+  badges.each{|badge| badges_and_room_assignments.push(badge)}
+  room_assignments.each{|room| badges_and_room_assignments.push(room)}
+  puts badges_and_room_assignments()
+  
+  
+    
+  
+end 
